c13a9a34aa1a48838739fd1ac44542fecc975b17
Tino Lind <tinolind066@gmail.com>
2026-04-10 17:17:33 +0000

    Force dynamic rendering for signup

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useState, Suspense } from "react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
